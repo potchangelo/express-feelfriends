ed740effedb9fd0e87185047531ae2f619088140
Potchara Puttawanchai <potchangelo@gmail.com>
2022-01-20 16:45:28 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8484;
+
+app.get('/', (req, res) => {
+    res.send('สวัสดีหน้าแรก');
+});
+
+app.listen(port, () => {
+    console.log(`มาค่ะ http://localhost:${port}`);
+});
