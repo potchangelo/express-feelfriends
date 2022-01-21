b96d248e5f0cf638fc064200be35d30bd9353122
Potchara Puttawanchai <potchangelo@gmail.com>
2022-01-21 17:40:13 +0700

    Route request query params

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 8484;
 
 app.get('/', (request, response) => {
-  response.send('สวัสดีหน้าแรก 555');
+  console.log(request.query);
+  const { q, sortBy } = request.query;
+  response.send(`สวัสดีหน้าแรก 555 - ค้นหาคำว่า ${q}, เรียงลำดับจาก ${sortBy}`);
+});
+
+app.get('/p/:postId', (request, response) => {
+  console.log(request.params);
+  const { postId } = request.params;
+  response.send(`สวัสดีหน้าโพสต์เดียว ID = ${postId}`);
 });
 
 app.listen(port, () => {
