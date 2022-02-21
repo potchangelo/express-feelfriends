41226a881fdb5ddd49080743021836d56537a52b
Potchara Puttawanchai <potchangelo@gmail.com>
2022-02-21 19:09:04 +0700

    Get all posts with comments count

--- routers/general.js
+++ routers/general.js
@@ -6,11 +6,18 @@ const router = express.Router();
 router.get('/', async (request, response) => {
   let allPosts = [];
   try {
-    allPosts = await db.select('*').from('post').orderBy('id', 'desc');
+    allPosts = await db
+      .select('post.id', 'post.title', 'post.from', 'post.createdAt')
+      .count('comment.id as commentsCount')
+      .from('post')
+      .leftJoin('comment', 'post.id', 'comment.postId')
+      .groupBy('post.id')
+      .orderBy('post.id', 'desc');
   }
   catch (error) {
     console.error(error);
   }
+  console.log(allPosts)
   response.render('home', { allPosts });
 });
 
