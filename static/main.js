5b8aeeff07cc55ac461474aac6ce85e3c0a34363
Potchara Puttawanchai <potchangelo@gmail.com>
2022-03-11 17:22:02 +0700

    Scroll top script

--- static/main.js
+++ static/main.js
@@ -1,1 +1,11 @@
 console.log('FewFriends');
+
+const scrollTopButton = document.querySelector('.scroll-top-button');
+
+function scrollTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+if (!!scrollTopButton) {
+  scrollTopButton.addEventListener('click', scrollTop);
+}
