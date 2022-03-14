b8a6fe8aacd217959c437fa3d495355f5380da68
Potchara Puttawanchai <potchangelo@gmail.com>
2022-03-14 15:24:28 +0700

    Config production port

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 require('./db');
 const generalRouter = require('./routers/general');
 const postsRouter = require('./routers/posts');
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Bangkok');
+
 const app = express();
-const port = process.env.APP_PORT ?? 8765;
+const port = process.env.APP_PORT ?? process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'hbs');
@@ -17,5 +24,10 @@ app.use('/', generalRouter);
 app.use('/p', postsRouter);
 
 app.listen(port, () => {
-  console.log(`มาค่ะ http://localhost:${port}`);
+  if (process.env.NODE_ENV === 'production') {
+    console.log(`เย้เฮ่ ${port}`);
+  }
+  else {
+    console.log(`มาค่ะ http://localhost:${port}`);
+  }
 });
