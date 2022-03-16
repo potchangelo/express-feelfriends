8c1351a915a221655ad446693e48e2b76cbd6d18
Potchara Puttawanchai <potchangelo@gmail.com>
2022-03-16 11:28:11 +0700

    More JS language

--- static/main.js
+++ static/main.js
@@ -1,4 +1,8 @@
 console.log('FeelFriends');
+console.log('ฮั่นแน่');
+console.log('เปิดดู Console อยู่หละสิ');
+console.log('console.log ตรงนี้จะแสดงผลในทุกหน้าเว็บนะครับ');
+console.log('แต่ตัว Function scrollTop จะทำงานเฉพาะในหน้าที่มีปุ่มลูกศร Scroll top เท่านั้น');
 
 const scrollTopButton = document.querySelector('.scroll-top-button');
 
